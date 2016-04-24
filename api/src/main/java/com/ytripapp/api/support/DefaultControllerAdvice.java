b3d50dad9e3efefe5b9b9ed32663aba5d4729824
Hong Zhu <hong.zhu.van@gmail.com>
2016-04-24 01:05:25 -0700

    refacotred

--- api/src/main/java/com/ytripapp/api/support/DefaultControllerAdvice.java
+++ api/src/main/java/com/ytripapp/api/support/DefaultControllerAdvice.java
@@ -1,10 +1,13 @@
 package com.ytripapp.api.support;
 
+import com.ytripapp.api.ApiError;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.Locale;
@@ -15,4 +18,12 @@ public class DefaultControllerAdvice extends ResponseEntityExceptionHandler {
     @Autowired
     protected MessageSource messageSource;
 
+    @ResponseBody
+    @ExceptionHandler({Throwable.class})
+    public ResponseEntity<ApiError> handleUncaughtException(Throwable t, Locale locale) {
+        ApiError error = new ApiError();
+        error.setCode("error.internalServer");
+        error.setMessage(messageSource.getMessage(error.getCode(), null, locale));
+        return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
