3df6e7190793f53a188090918de5cadb441c10d2
Hong Zhu <hong.zhu.van@gmail.com>
2016-04-26 20:55:21 -0700

    refactored

--- gateway/src/main/java/com/ytripapp/gateway/security/AuthenticationFailureException.java
+++ gateway/src/main/java/com/ytripapp/gateway/security/AuthenticationFailureException.java
@@ -4,6 +4,8 @@ import org.springframework.security.core.AuthenticationException;
 
 public class AuthenticationFailureException extends AuthenticationException {
 
+    private static final long serialVersionUID = -3253425408376334320L;
+
     public AuthenticationFailureException(Throwable t) {
         super("Invalid username or password", t);
     }
